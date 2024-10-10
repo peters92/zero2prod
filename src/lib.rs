dc294d2143bab9f96b4bac033793851f7b2db96c
Peter S. <24956712+peters92@users.noreply.github.com>
2024-10-10 14:52:25 +0200

    Add first intergration test

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
+
+async fn health_check() -> HttpResponse {
+    // return a 200 OK response
+    HttpResponse::Ok().finish()
+}
+
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run();
+
+    Ok(server)
+}
