67c84b1ccd2a2b2415173fa61d7855a5dd24fa69
Peter S. <24956712+peters92@users.noreply.github.com>
2024-10-10 13:20:54 +0200

    Run linter

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
-use actix_web::{http::StatusCode, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
 
-async fn health_check(req: HttpRequest) -> impl Responder {
+async fn health_check() -> impl Responder {
     // return a 200 OK response
     HttpResponse::Ok()
 }
 
-
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error>{
+async fn main() -> Result<(), std::io::Error> {
     HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/", web::get().to(greet))
-            // .route("/{name}", web::get().to(greet))  
+        // .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
     .run()
