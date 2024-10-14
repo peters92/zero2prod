8b0ca0d67d9ae4f398ec6050a5181b99d0a99587
Peter S <pese@tdcnet.dk>
2024-10-14 14:48:02 +0200

    Update integration tests with subscriptions
    Added POST subscribe route handler to lib to allow tests to pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
+use actix_web::{dev::Server, web::{self, Form}, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
     // return a 200 OK response
     HttpResponse::Ok().finish()
 }
 
-pub fn run(address: &str) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind(address)?
-        .run();
 
+#[derive(serde::Deserialize, Debug)]
+struct FormData {
+    email: String,
+    name: String
+}
+
+async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
+    println!("Deserialized FormData: {:?}", form);
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,22 +1,80 @@
 use reqwest;
+use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let server_address = spawn_app();
     let client = reqwest::Client::new();
 
+    // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(format!("{}/health_check", server_address))
         .send()
         .await
         .expect("Failed to execute request");
 
+    // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address.");
+#[tokio::test]
+async fn subscribe_return_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    // Act
+    let body = "name=Le%20Guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(format!("{}/subscriptions", app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Should be able to run post request.");
+    // Assert
+    assert_eq!(200, response.status().as_u16())
+}
+
+#[tokio::test]
+async fn subscribe_return_a_400_when_data_is_missing() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=Le%20Guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email")
+
+    ];
+    // Act
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Should be able to run post request.");
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the request body was {}",
+            error_message
+        )
+    }
+}
+
+/// Create an application instance
+/// and return the local address with randomly bound port - "127.0.0.1:PORT"
+fn spawn_app() -> String {
+    let listener =
+        TcpListener::bind("127.0.0.1:0").expect("We should be able to bind a random port.");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address.");
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
