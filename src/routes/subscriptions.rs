0514d9bca9414daca003ab73c635b9ceac8c35fa
Peter S. <24956712+peters92@users.noreply.github.com>
2024-10-21 10:37:35 +0200

    Test PR for CI (#3)
    * Add general address changes
    
    * Update integration tests with subscriptions
    
    Added POST subscribe route handler to lib to allow tests to pass
    
    * Add --force flag to CI dependency installs
    
    * Add shell script for spinning up posgtre db with Docker
    
    * Update project structure
    
    * Update configurations and tests with database connection
    
    * Update with new cargo.lock and lib.rs without conflicts
    
    * Add test app and database setup to integration tests
    
    All integrations tests pass at this point
    
    * Update CI pipeline with sqlx offline mode and rust toolchain for caching
    
    * Update tracing
    
    * Fix linting with compiler dereference issue
    
    * Add sqlx cached query for offline database testing
    
    * Update sqlx offline queries with all targets
    
    ---------
    
    Co-authored-by: Peter S <pese@tdcnet.dk>

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,53 @@
+use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
+
+#[derive(serde::Deserialize, Debug)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, db_pool),
+    fields(
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
+pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> HttpResponse {
+    match insert_subscriber(&form, &db_pool).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+#[tracing::instrument(
+        name = "Inserting subscriber data into database"
+        skip(form, db_pool),
+        )
+]
+pub async fn insert_subscriber(
+    form: &web::Form<FormData>,
+    db_pool: &web::Data<PgPool>,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(db_pool.get_ref())
+    .await
+    .map_err(|err| {
+        tracing::error!("Error while inserting subscriber into database: {}", err);
+        err
+    })?;
+    Ok(())
+}
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,15 +1,96 @@
 use reqwest;
+use secrecy::{ExposeSecret, Secret};
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
+use std::sync::LazyLock;
+use uuid::Uuid;
+use zero2prod::configuration::{self, DatabaseSettings};
+use zero2prod::telemetry;
+
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
+static TRACING: LazyLock<()> = LazyLock::new(|| {
+    let default_filter_lever = "info".to_string();
+    let subscriber_name = "debug".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber =
+            telemetry::get_subscriber(subscriber_name, default_filter_lever, std::io::stdout);
+        telemetry::init_subscriber(subscriber);
+    } else {
+        let subscriber =
+            telemetry::get_subscriber(subscriber_name, default_filter_lever, std::io::sink);
+        telemetry::init_subscriber(subscriber);
+    }
+});
+
+/// Create an application instance
+/// and return the local address with randomly bound port - "127.0.0.1:PORT"
+async fn spawn_app() -> TestApp {
+    LazyLock::force(&TRACING);
+
+    let listener =
+        TcpListener::bind("127.0.0.1:0").expect("We should be able to bind a random port.");
+    let port = listener.local_addr().unwrap().port();
+
+    let mut config =
+        configuration::get_configuration().expect("We should have a config file available");
+    config.database.database_name = Uuid::new_v4().to_string();
+
+    let connection_pool = configure_database(&config.database).await;
+
+    let server = zero2prod::startup::run(listener, connection_pool.clone())
+        .expect("Failed to bind address.");
+    let _ = tokio::spawn(server);
+
+    TestApp {
+        address: format!("http://127.0.0.1:{}", port),
+        db_pool: connection_pool,
+    }
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    // Create the test database
+    let maintenance_settings = DatabaseSettings {
+        database_name: "postgres".to_string(),
+        username: "postgres".to_string(),
+        password: Secret::new("password".to_string()),
+        ..config.clone()
+    };
+    let mut connection =
+        PgConnection::connect(&maintenance_settings.connection_string().expose_secret())
+            .await
+            .expect("We should be able to connect to the maintenance database.");
+
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("We should be able to create a test database through the maintenance db.");
+
+    // Migrate database
+    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
+        .await
+        .expect("We should be able to create the database pool at this point.");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Migrations should be able to run");
+
+    connection_pool
+}
 
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    let server_address = spawn_app();
+    let test_app = spawn_app().await;
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get(format!("{}/health_check", server_address))
+        .get(format!("{}/health_check", test_app.address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -22,36 +103,45 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_return_a_200_for_valid_form_data() {
     // Arrange
-    let app_address = spawn_app();
+    let test_app = spawn_app().await;
     let client = reqwest::Client::new();
+
     // Act
-    let body = "name=Le%20Guin&email=ursula_le_guin%40gmail.com";
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
-        .post(format!("{}/subscriptions", app_address))
+        .post(format!("{}/subscriptions", test_app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
         .await
         .expect("Should be able to run post request.");
     // Assert
-    assert_eq!(200, response.status().as_u16())
+    assert_eq!(200, response.status().as_u16());
+
+    // check the database for added test subscriber
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions LIMIT 1",)
+        .fetch_one(&test_app.db_pool)
+        .await
+        .expect("We should be able to query the database.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
 async fn subscribe_return_a_400_when_data_is_missing() {
     // Arrange
-    let app_address = spawn_app();
+    let test_app = spawn_app().await;
     let client = reqwest::Client::new();
     let test_cases = vec![
-        ("name=Le%20Guin", "missing the email"),
+        ("name=le%20guin", "missing the email"),
         ("email=ursula_le_guin%40gmail.com", "missing the name"),
-        ("", "missing both name and email")
-
+        ("", "missing both name and email"),
     ];
     // Act
     for (invalid_body, error_message) in test_cases {
         let response = client
-            .post(format!("{}/subscriptions", app_address))
+            .post(format!("{}/subscriptions", test_app.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .body(invalid_body)
             .send()
@@ -65,16 +155,4 @@ async fn subscribe_return_a_400_when_data_is_missing() {
             error_message
         )
     }
-}
-
-/// Create an application instance
-/// and return the local address with randomly bound port - "127.0.0.1:PORT"
-fn spawn_app() -> String {
-    let listener =
-        TcpListener::bind("127.0.0.1:0").expect("We should be able to bind a random port.");
-    let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address.");
-    let _ = tokio::spawn(server);
-
-    format!("http://127.0.0.1:{}", port)
 }
